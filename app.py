054547d82c9a108c5d3c5a477c36329189dff3bd
naruto072 <92872414+naruto072@users.noreply.github.com>
2023-09-27 17:06:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,232 +1,33 @@
-from flask import Flask, render_template, Response, request
-import cv2
-import datetime, time
-import os, sys
-import numpy as np
-from threading import Thread
-
-import pickle
-
-import mediapipe as mp
-
-global capture,rec_frame, grey, switch, neg, face, rec, out 
-capture=0
-grey=0
-neg=0
-face=0
-switch=1
-rec=0
-model_dict = pickle.load(open('./model.p', 'rb'))
-model = model_dict['model']
-model2 = model_dict['model2']
-
-cap = cv2.VideoCapture(0)
-
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=.2,max_num_hands=2)
-
-import json
-#make shots directory to save pics
-try:
-    os.mkdir('./shots')
-except OSError as error:
-    pass
-
-#Load pretrained face detection model    
-net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
-
-#instatiate flask app  
-app = Flask(__name__, template_folder='./templates')
+from detect import detect
+from flask import Flask, json, request, jsonify
+import os,json,pickle
+import urllib.request
+from werkzeug.utils import secure_filename
 
 
-camera = cv2.VideoCapture(0)
+app = Flask(__name__)
 
-def record(out):
-    global rec_frame
-    while(rec):
-        time.sleep(0.05)
-        out.write(rec_frame)
+app.secret_key = "caircocoders-ednalan"
 
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-def detect_face(frame):
-    global net
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-        (300, 300), (104.0, 177.0, 123.0))   
-    net.setInput(blob)
-    detections = net.forward()
-    confidence = detections[0, 0, 0, 2]
-
-    if confidence < 0.5:            
-            return frame           
-
-    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
-    try:
-        frame=frame[startY:endY, startX:endX]
-        (h, w) = frame.shape[:2]
-        r = 480 / float(h)
-        dim = ( int(w * r), 480)
-        frame=cv2.resize(frame,dim)
-    except Exception as e:
-        pass
-    return frame
- 
-
-def gen_frames():  # generate frame by frame from camera
-    global out, capture,rec_frame
-    while True:
-        data_aux = []
-        x_ = []
-        y_ = []
-
-        ret, frame = cap.read()
-
-        H, W, _ = frame.shape
-        h,w,_ = frame.shape
-
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        results = hands.process(frame_rgb)
-        if results.multi_hand_landmarks:
-            for handLMs in results.multi_hand_landmarks:
-                x_max = 0
-                y_max = 0
-                x_min = w
-                y_min = h
-                for lm in handLMs.landmark:
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x 
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-                mp_drawing.draw_landmarks(frame, handLMs, mp_hands.HAND_CONNECTIONS)
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame,  # image to draw
-                    hand_landmarks,  # model output
-                    mp_hands.HAND_CONNECTIONS,  # hand connections
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
 
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-
-                    x_.append(x)
-                    y_.append(y)
-
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
-
-            x1 = int(min(x_) * W) - 10
-            y1 = int(min(y_) * H) - 10
-
-            x2 = int(max(x_) * W) - 10
-            y2 = int(max(y_) * H) - 10
-            files = {'image':('test.png',np.asarray(data_aux).tobytes()),'Content-Type':'image/png','Content-Length':1}
-        
-            if len(data_aux) == 42:
-                prediction = model.predict([np.asarray(data_aux)])#json.loads(r.text)['prediction']
-            else:
-                prediction = model2.predict([np.asarray(data_aux)])#json.loads(r.text)['prediction']
-            # r = requests.post(url,data = json.dumps(data_aux))
-            # print(r.text)
-            
-            # print(prediction)
-            #prediction = '0'
-
-            predicted_character = prediction[0]#labels_dict[int(prediction)]
-
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-            cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-                        cv2.LINE_AA)
-
-                
-            try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
-                
-        else:
-            pass
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-    
-    
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/requests',methods=['POST','GET'])
-def tasks():
-    global switch,camera
-    if request.method == 'POST':
-        if request.form.get('click') == 'Capture':
-            global capture
-            capture=1
-        elif  request.form.get('grey') == 'Grey':
-            global grey
-            grey=not grey
-        elif  request.form.get('neg') == 'Negative':
-            global neg
-            neg=not neg
-        elif  request.form.get('face') == 'Face Only':
-            global face
-            face=not face 
-            if(face):
-                time.sleep(4)   
-        elif  request.form.get('stop') == 'Stop/Start':
-            
-            if(switch==1):
-                switch=0
-                camera.release()
-                cv2.destroyAllWindows()
-                
-            else:
-                camera = cv2.VideoCapture(0)
-                switch=1
-        elif  request.form.get('rec') == 'Start/Stop Recording':
-            global rec, out
-            rec= not rec
-            if(rec):
-                now=datetime.datetime.now() 
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
-                #Start new thread for recording the video
-                thread = Thread(target = record, args=[out,])
-                thread.start()
-            elif(rec==False):
-                out.release()
-                          
-                 
-    elif request.method=='GET':
-        return render_template('index.html')
-    return render_template('index.html')
-
-
+@app.route('/api/upload', methods=['POST','GET'])
+def upload_image():
+	d = pickle.loads(json.loads(request.data).encode('latin-1'))
+	res = detect(d)
+	resp = jsonify({
+			'prediction' : res,
+			'status' : True,
+			'message' : 'Images successfully uploaded'})
+	resp.status_code = 200
+	return resp
+	
+	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=7229)
-    
-camera.release()
-cv2.destroyAllWindows()     
+    app.run(debug=True)
