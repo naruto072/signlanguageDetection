ba91200c02373e2aeb000c693729ecf9b8e12205
naruto072 <92872414+naruto072@users.noreply.github.com>
2023-10-13 13:32:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,15 @@
 from flask import Flask, json, request, jsonify
 import os,json,pickle
 import urllib.request
-from werkzeug.utils import secure_filename
+
 
 
 app = Flask(__name__)
 
 app.secret_key = "caircocoders-ednalan"
 
-#UPLOAD_FOLDER = 'static/uploads'
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 
 
@@ -30,4 +27,4 @@ def upload_image():
 	
 	
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
