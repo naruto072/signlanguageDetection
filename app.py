a5bc16ee522029f6c3dbeaad7671ed4a5378399f
naruto072 <92872414+naruto072@users.noreply.github.com>
2023-10-13 13:25:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 app.secret_key = "caircocoders-ednalan"
 
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#UPLOAD_FOLDER = 'static/uploads'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
